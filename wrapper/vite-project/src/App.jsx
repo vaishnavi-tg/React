d89bd3a8bb80174a32031599dbf342951a49fbfd
Vaishnavi T.G <vaishnavitg2@gmail.com>
2025-03-24 16:44:08 +0530

    Added wrapper and memo

--- wrapper/vite-project/src/App.jsx
+++ wrapper/vite-project/src/App.jsx
@@ -1,27 +1,31 @@
 function App(){
   return(
     <div>
-      <CardWrapper innercomponent={TextComponent}/>
+      <CardWrapper>
+        <h5> Hiiii their</h5>
+      </CardWrapper>
+  
+    <CardWrapper>
+      <h1> Hiiii their2</h1>
+    </CardWrapper>
     </div>
   )
 }
 
 
-function CardWrapper({innercomponent}){
+function CardWrapper({children}){
   return(
       <div style={{
-        border:"2px solid black", padding:'5px'
-
-      }}>
-      {innercomponent}
+        border:"2px solid black", padding:'5px'}}>
+      {children}
     </div>
   )
 }
 
-function TextComponent(){
-  return <div>
-    hi their
-  </div>
-}
+// function TextComponent(){
+//   return <div>
+//     hi their
+//   </div>
+// }
 
 export default App
